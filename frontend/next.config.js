bcb84e7ffafff4d3b71bdcd425d6a7d47fb0ff8d
Sidak Singh <sidaksingh318@gmail.com>
2025-07-31 19:39:36 -0400

    soem config

---
+++ frontend/next.config.js
@@ -0,0 +1,65 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    // Allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/dashboard/competitor/competitions",
+        destination: "/competitions",
+        permanent: false,
+      },
+      {
+        source: "/dashboard/competitor/applications",
+        destination: "https://tally.so/r/mOxbXY",
+        permanent: false,
+      },
+      {
+        source: "/dashboard/competitor/profile",
+        destination: "/under-construction",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://pitchdeck-ddnd.onrender.com/api/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Accept, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
